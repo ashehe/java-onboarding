f1e0baccb59bb1ca33eb531ca59ce46f19adaad2
ash96235@gmail.com <ash96235@gmail.com>
2023-09-10 10:49:37 +0900

    Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+        for (int j = 0; j < cryptogram.length() - 1; j++) {
+            for (int i = 0; i < cryptogram.length() - 1; i++) {
+                if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+                    cryptogram = cryptogram.substring(0, i) + cryptogram.substring(i + 2);
+                    if (cryptogram.length() == 2) {
+                        if (cryptogram.charAt(0) == cryptogram.charAt(1)) {
+                            cryptogram = "";
+                        }
+                    }
+                }
+            }
+        }
+
+        return cryptogram;
     }
 }
