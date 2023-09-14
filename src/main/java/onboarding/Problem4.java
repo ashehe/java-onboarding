f17d2e449d76ecb9c61ba3992f8e9ec4ded602ac
ash96235@gmail.com <ash96235@gmail.com>
2023-09-14 22:22:29 +0900

    solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        for (int i = 0; i < word.length(); i++) {
+            if ( 65 <= (int)word.charAt(i) && (int)word.charAt(i) <= 90) {
+                word = word.substring(0, i) + (char)(155 - (int)word.charAt(i)) + word.substring(i + 1);
+            } else if ( 97 <= (int)word.charAt(i) && (int)word.charAt(i) <= 122) {
+                word = word.substring(0, i) + (char)(219 - (int)word.charAt(i)) + word.substring(i + 1);
+            }
+        }
+        String answer = word;
         return answer;
     }
 }
